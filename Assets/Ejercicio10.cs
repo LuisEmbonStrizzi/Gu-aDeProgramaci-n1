8eb3313e073e5d70599d3e3bbb98de0e61c737bf
LuisEmbonStrizzi <101569004+LuisEmbonStrizzi@users.noreply.github.com>
2022-03-28 20:03:07 -0300

    Exercise 10 finished and 11 almost

---
+++ Assets/Ejercicio10.cs
@@ -0,0 +1,46 @@
+﻿//10.Realizá un programa que resuelva el siguiente problema: Nos piden desarrollar un
+//programa que informe si un estacionamiento se encuentra o no abierto según la hora
+//ingresada por el usuario.
+//Nosotros sabemos que el estacionamiento se encuentra abierto entre las 10 y las 18hs.
+//(Crear constante HORA_APERTURA = 10, y HORA_CIERRE = 18).
+//Al iniciar el programa, se tomará como hora actual el valor de una variable expuesta en el
+//Inspector, y deberemos setear una variable booleana llamada estaAbierto en true o false,
+//según corresponda, e informar por pantalla mediante la misma si el estacionamiento se
+//encuentra abierto o no. Obs: En el caso de que el valor de la variable correspona a una hora
+//menor a 0 o mayor a 24, mostrar el mensaje: “Ha ingresado una hora incorrecta”
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio10 : MonoBehaviour
+{
+    public int hora;
+    int HORA_APERTURA = 10;
+    int HORA_CIERRE = 18;
+    bool estaAbierto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (hora >= HORA_APERTURA && hora <= HORA_CIERRE) 
+        {
+            estaAbierto = true;
+            Debug.Log(estaAbierto + " ,es decir, que el estacionamiento se encuentra abierto");
+        }
+        else if (hora >= 0 && hora < HORA_APERTURA || hora > HORA_CIERRE && hora <= 24)
+        {
+            estaAbierto = false;
+            Debug.Log(estaAbierto + " ,es decir, que el estacionamiento no se encuentra abierto");
+        }
+        else if (hora < 0 || hora > 24)
+        {
+            Debug.Log("Ha ingresado una hora incorrecta");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ejercicio11.cs
@@ -0,0 +1,40 @@
+﻿//11.Realizá un programa que resuelva el siguiente problema:
+//Deberá ingresarse por Inspector una fecha de compra (String en el siguiente formato:
+//YYYYMMDD), un nombre de comprador, un nombre de producto y una cantidad y precio del
+//producto comprado. Mostrá a modo de ticket, la información ingresada y el monto a pagar.
+//Modelo de Ticket:
+//Fecha de Compra: YYYYMMDD
+//Nombre del Comprador: xxxxx xxxxx
+//Producto solicitado: xxxxx
+//Cantidad solicitada: xx
+//Precio Unitario: $xxx
+//Total a Pagar: $xxxxx
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio11 : MonoBehaviour
+{
+    public string FechaDeCompra;
+    public string NombreDelComprador;
+    public string NombreDelProducto;
+    public int Cantidad;
+    public int Precio;
+
+    int Total;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Total = Precio * Cantidad;
+
+        Debug.Log(
+              "  Fecha de compra: " + FechaDeCompra + "\n" + " Nombre del comprador: " + NombreDelComprador + "\n" + " Producto solicitado: " + NombreDelProducto + "\n" + " Cantidad solicitada: " + Cantidad + " Precio unitario: " + Precio + " Total a pagar: " + Total);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
